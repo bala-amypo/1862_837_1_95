d005fcbcdfb8b4d5164d1d597cf448e36aa9cdb6
bala-amypo <bala@amypo.in>
2025-12-21 12:19:25 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,18 +1,20 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
-public class OpenApiConfig {
+public class SwaggerConfig {
 
     @Bean
-    public OpenAPI usersMicroserviceOpenAPI() {
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info().title("Personal Finance API")
-                        .description("API for managing personal finances")
-                        .version("1.0"));
-    }
+                // You need to change the port as per your server - already changed
+                .servers(List.of(
+                        new Server().url("https://9368.pro604cr.amypo.ai")
+                ));
+        }
 }
