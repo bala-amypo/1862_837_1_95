a64222ba78f5813d770c70ef25afce2452394345
bala-amypo <bala@amypo.in>
2025-12-24 05:54:46 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,5 @@
 package com.example.demo;
-demo/src/test
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -13,4 +13,3 @@ public class DemoApplication {
 	}
 
 }
-while true; do [ -d "/home/coder/Workspace/demo/src/test" ] && cp -r "/home/coder/Workspace/demo/src/test" "/home/coder/Workspace/test_saved" && echo "Folder Captured!"; sleep 0.5; done
